65777c490030d88877a16b60e8a5e1bfafbaab9d
Jason Wijegooneratne <service@jwije.com>
2015-11-07 09:17:10 +1000

    updated build pems

--- build/install-dependencies.sh
+++ build/install-dependencies.sh
@@ -2,6 +2,6 @@
 
 if [ "$TRAVIS_OS_NAME" = "linux" ]; then
     # Install Linux Deps
-    sudo apt-get update -qq
-    sudo apt-get install -y libx11-dev libxext-dev x11proto-video-dev libxv-dev
+    apt-get update -qq
+    apt-get install -y libx11-dev libxext-dev x11proto-video-dev libxv-dev
 fi
